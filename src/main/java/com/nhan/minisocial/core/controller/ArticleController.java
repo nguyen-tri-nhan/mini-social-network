205a94b8c1e3eea2d9a0afe9371a7a740d71e091
nguyentrinhan-dev <nguyentrinhan.dev@gmail.com>
2021-03-01 23:58:48 +0700

    done save api

--- src/main/java/com/nhan/minisocial/core/controller/ArticleController.java
+++ src/main/java/com/nhan/minisocial/core/controller/ArticleController.java
@@ -4,6 +4,7 @@ package com.nhan.minisocial.core.controller;
 import com.nhan.minisocial.core.api.Api;
 import com.nhan.minisocial.core.entity.Article;
 import com.nhan.minisocial.core.facade.ArticleFacade;
+import com.nhan.minisocial.core.payload.ApiResponse;
 import com.nhan.minisocial.core.resource.ArticleResource;
 import com.nhan.minisocial.core.security.CurrentUser;
 import com.nhan.minisocial.core.security.UserPrincipal;
@@ -12,8 +13,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 public class ArticleController {
@@ -21,9 +24,12 @@ public class ArticleController {
     @Autowired
     private ArticleFacade articleFacade;
 
-    @PostMapping(Api.ArticleApi.GET)
+    @PostMapping(Api.ArticleApi.POST)
     public ResponseEntity<?> postArticle(@CurrentUser UserPrincipal user, @Valid @RequestBody ArticleResource articleResource){
         Article article = articleFacade.createOrUpdate(articleResource, user);
-        return null;
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path(Api.ArticleApi.GET)
+                .buildAndExpand(article.getId()).toUri();
+        return ResponseEntity.created(location).body(new ApiResponse(true, "Article saved successfully"));
     }
 }
