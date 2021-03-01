c96de8acced6d3dd5f172bc14846c430678cc470
nguyentrinhan-dev <nguyentrinhan.dev@gmail.com>
2021-03-01 23:38:01 +0700

    temporary

--- src/main/java/com/nhan/minisocial/core/controller/ArticleController.java
+++ src/main/java/com/nhan/minisocial/core/controller/ArticleController.java
@@ -3,9 +3,11 @@ package com.nhan.minisocial.core.controller;
 
 import com.nhan.minisocial.core.api.Api;
 import com.nhan.minisocial.core.entity.Article;
+import com.nhan.minisocial.core.facade.ArticleFacade;
 import com.nhan.minisocial.core.resource.ArticleResource;
 import com.nhan.minisocial.core.security.CurrentUser;
 import com.nhan.minisocial.core.security.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +18,12 @@ import javax.validation.Valid;
 @RestController
 public class ArticleController {
 
+    @Autowired
+    private ArticleFacade articleFacade;
+
     @PostMapping(Api.ArticleApi.GET)
     public ResponseEntity<?> postArticle(@CurrentUser UserPrincipal user, @Valid @RequestBody ArticleResource articleResource){
+        Article article = articleFacade.createOrUpdate(articleResource, user);
         return null;
     }
 }
