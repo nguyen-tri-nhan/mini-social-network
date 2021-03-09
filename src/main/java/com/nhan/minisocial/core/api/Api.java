17673ca06a93c2eff7347aab74009a1dad65086c
nguyentrinhan-dev <nguyentrinhan.dev@gmail.com>
2021-03-09 22:59:32 +0700

    init cmt

--- src/main/java/com/nhan/minisocial/core/api/Api.java
+++ src/main/java/com/nhan/minisocial/core/api/Api.java
@@ -2,9 +2,16 @@ package com.nhan.minisocial.core.api;
 
 public class Api {
     public static final String API = "/api";
-    public static class ArticleApi{
+
+    public static class ArticleApi {
         public static final String COLLECTION = API + "/articles";
         public static final String POST = COLLECTION + "/POST";
         public static final String GET = COLLECTION + "/{id}";
+    }
+
+    public static class VoteApi {
+        public static final String COLLECTION = API + "/votes";
+        public static final String VOTE_ARTICLE = COLLECTION + "/{postId}";
+        public static final String VOTE_COMMENT = COLLECTION + "/{commentId}";
     }
 }
--- src/main/java/com/nhan/minisocial/core/controller/VoteController.java
+++ src/main/java/com/nhan/minisocial/core/controller/VoteController.java
@@ -1,7 +1,20 @@
 package com.nhan.minisocial.core.controller;
 
+import com.nhan.minisocial.core.api.Api;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VoteController {
+
+    @PostMapping(Api.VoteApi.VOTE_ARTICLE)
+    public void voteArticle(@PathVariable(name = "postId") long postId){
+
+    }
+
+    @PostMapping(Api.VoteApi.VOTE_COMMENT)
+    public void voteComment(@PathVariable(name = "commentId") long commentId){
+
+    }
 }
