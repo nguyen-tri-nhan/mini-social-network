b261b4f258d9930c297cc92a224b9979095415a1
nguyentrinhan-dev <nguyentrinhan.dev@gmail.com>
2021-03-18 22:12:53 +0700

    update

--- src/main/java/com/nhan/minisocial/core/service/CommentResourceService.java
+++ src/main/java/com/nhan/minisocial/core/service/CommentResourceService.java
@@ -2,17 +2,27 @@ package com.nhan.minisocial.core.service;
 
 import com.nhan.minisocial.core.entity.Comment;
 import com.nhan.minisocial.core.resource.CommentResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentResourceService {
+
+    @Autowired
+    private UserResourceService userResourceService;
+
     private Comment toEntity() {
         // TODO: impl here
         return null;
     }
 
     private CommentResource toResource(Comment comment){
-        return null;
+        CommentResource resource = new CommentResource();
+        resource.setId((comment.getId()));
+        resource.setArticleId(comment.getArticle().getId());
+        resource.setDescription(comment.getDescription());
+        resource.setUser(userResourceService.getUser(comment.getUser().getId()));
+        return resource;
     }
 
     public CommentResource commentAnArticle() {
