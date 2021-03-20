b2c7c75a5250cf784241f2db6f0488bfa4f53e3b
nguyentrinhan-dev <nguyentrinhan.dev@gmail.com>
2021-03-21 01:03:17 +0700

    update

--- src/main/java/com/nhan/minisocial/core/facade/CommentFacade.java
+++ src/main/java/com/nhan/minisocial/core/facade/CommentFacade.java
@@ -2,10 +2,16 @@ package com.nhan.minisocial.core.facade;
 
 import com.nhan.minisocial.core.payload.CommentRequest;
 import com.nhan.minisocial.core.security.UserPrincipal;
+import com.nhan.minisocial.core.service.CommentResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentFacade {
+
+    @Autowired
+    private CommentResourceService commentResourceService;
+
     public void commentAPost(UserPrincipal user, long articleId, CommentRequest comment) {
 
     }
