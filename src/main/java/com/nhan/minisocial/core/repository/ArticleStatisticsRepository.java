b869e1f5653b47b1474555dcca03c4c5e409817d
nguyentrinhan-dev <nguyentrinhan.dev@gmail.com>
2021-05-02 21:41:51 +0700

    import repository for arstatistics

---
+++ src/main/java/com/nhan/minisocial/core/repository/ArticleStatisticsRepository.java
@@ -0,0 +1,7 @@
+package com.nhan.minisocial.core.repository;
+
+import com.nhan.minisocial.core.entity.ArticleStatistics;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleStatisticsRepository extends JpaRepository<ArticleStatistics, Long> {
+}
