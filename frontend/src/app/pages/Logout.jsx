104aa86f7c04bd6f347e7bde1ce06d4d47766458
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-08-07 21:44:30 +0700

    logout page

--- frontend/src/app/pages/Logout.jsx
+++ frontend/src/app/pages/Logout.jsx
@@ -1,5 +1,9 @@
 import MAuth from '../model/MAuth';
 
+/**
+ * Null page to destroy auth in context and cookie.
+ * @returns route to login page
+ */
 const Logout = () => {
   MAuth.logout();
   window.location.href = '/login';
