e167ab5c6b44d2073e970de108fa325efcd0aa79
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-08-15 14:39:23 +0700

    rework signup page

--- frontend/src/app/pages/Login.jsx
+++ frontend/src/app/pages/Login.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import {
-    Button,
-    Checkbox,
-    Container,
-    CssBaseline,
-    FormControlLabel,
-    Grid,
-    Link,
-    TextField,
-    Typography
+	Button,
+	Checkbox,
+	Container,
+	CssBaseline,
+	FormControlLabel,
+	Grid,
+	Link,
+	TextField,
+	Typography
 } from '@material-ui/core';
 import MAuth from '../model/MAuth'
 import { useHistory } from 'react-router-dom';
@@ -16,85 +16,85 @@ import RouteConstants from '../routes/RouteConstants';
 
 export default function Login() {
 
-    const history = useHistory();
+	const history = useHistory();
 
-    const [usernameOrEmail, setUsernameOrEmail] = useState('');
-    const [password, setPassword] = useState('');
+	const [usernameOrEmail, setUsernameOrEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-    const onSubmit = (e) => {
-        MAuth.login({usernameOrEmail, password})
-            .then(() => history.push("/"));
-    }
+	const onSubmit = (e) => {
+		MAuth.login({ usernameOrEmail, password })
+			.then(() => MAuth.isLoggedIn() && history.push("/"));
+	}
 
-    const onUserNameChange = (e) => {
-        setUsernameOrEmail(e.target.value);
-    }
+	const onUserNameChange = (e) => {
+		setUsernameOrEmail(e.target.value);
+	}
 
-    const onPasswordChange = (e) => {
-        setPassword(e.target.value);
-    }
-    return (
-        <div>
-            <Container component="main" maxWidth="xs">
-            <CssBaseline/>
-            <div className="login-box">
-                <Typography component="h1" variant="h5">
-                    Đăng nhập
-                </Typography>
-                <form className="login-form" noValidate>
-                    <TextField
-                        onChange={onUserNameChange}
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="username"
-                        label="Tên đăng nhập hoặc email"
-                        name="username"
-                        autoComplete="username"
-                        autoFocus
-                    />
-                    <TextField
-                        onChange={onPasswordChange}
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Mật khẩu"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary"/>}
-                        label="Ghi nhớ tài khoản"
-                    />
-                    <Button
-                        // type="submit"
-                        onClick={onSubmit}
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className="btn-login"
-                    >
-                        Đăng nhập
-                    </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Quên mật khẩu?
-                            </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link href= {RouteConstants.signup} variant="body2">
-                                {"Chưa có tài khoản? Đăng ký ngay"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-        </Container>
-        </div>
-    );
+	const onPasswordChange = (e) => {
+		setPassword(e.target.value);
+	}
+	return (
+		<div>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<div className="login-box">
+					<Typography component="h1" variant="h5">
+						Đăng nhập
+					</Typography>
+					<form className="login-form" noValidate>
+						<TextField
+							onChange={onUserNameChange}
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							id="username"
+							label="Tên đăng nhập hoặc email"
+							name="username"
+							autoComplete="username"
+							autoFocus
+						/>
+						<TextField
+							onChange={onPasswordChange}
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Mật khẩu"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+						/>
+						<FormControlLabel
+							control={<Checkbox value="remember" color="primary" />}
+							label="Ghi nhớ tài khoản"
+						/>
+						<Button
+							// type="submit"
+							onClick={onSubmit}
+							fullWidth
+							variant="contained"
+							color="primary"
+							className="btn-login"
+						>
+							Đăng nhập
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href="#" variant="body2">
+									Quên mật khẩu?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href={RouteConstants.signup} variant="body2">
+									{"Chưa có tài khoản? Đăng ký ngay"}
+								</Link>
+							</Grid>
+						</Grid>
+					</form>
+				</div>
+			</Container>
+		</div>
+	);
 }
--- frontend/src/app/pages/SignUp.jsx
+++ frontend/src/app/pages/SignUp.jsx
@@ -1,120 +1,180 @@
 import { useState } from "react"
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    Col,
-    Container,
-    Row,
-    CardColumns,
-    CardFooter,
-    Form,
-    FormGroup,
-    Input,
-    Label,
-} from "reactstrap";
+	Button,
+	Checkbox,
+	Container,
+	CssBaseline,
+	FormControlLabel,
+	Grid,
+	Link,
+	TextField,
+	Typography,
+	Box
+} from '@material-ui/core';
 import Service from "../service/Service"
 
 export default function SignUp() {
 
-    const [user, setUser] = useState({
-        username: '',
-        firstname: '',
-        lastname: '',
-        email: '',
-        password: '',
-        avatar: 'default.jpg',
-    });
+	const [user, setUser] = useState({
+		username: '',
+		firstname: '',
+		lastname: '',
+		email: '',
+		password: '',
+		avatar: 'default.jpg',
+	});
 
-    const [username, setUsername] = useState('');
-    const [firstname, setFirstname] = useState('');
-    const [lastname, setLastname] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [rePassword, setRePassword] = useState('');
+	const [username, setUsername] = useState('');
+	const [firstname, setFirstname] = useState('');
+	const [lastname, setLastname] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [rePassword, setRePassword] = useState('');
 
-    const onUserNameChange = (e) => {
-        setUsername(e.target.value);
-    };
+	const onUserNameChange = (e) => {
+		setUsername(e.target.value);
+	};
 
-    const onFirstnameChange = (e) => {
-        setFirstname(e.target.value);
-    };
+	const onFirstnameChange = (e) => {
+		setFirstname(e.target.value);
+	};
 
-    const onLastnameChange = (e) => {
-        setLastname(e.target.value);
-    }
+	const onLastnameChange = (e) => {
+		setLastname(e.target.value);
+	}
 
-    const onEmailChange = (e) => {
-        setEmail(e.target.value);
-    }
+	const onEmailChange = (e) => {
+		setEmail(e.target.value);
+	}
 
-    const onPasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+	const onPasswordChange = (e) => {
+		setPassword(e.target.value);
+	};
 
-    const onRePasswordChange = (e) => {
-        setRePassword(e.target.value);
-    };
+	const onRePasswordChange = (e) => {
+		setRePassword(e.target.value);
+	};
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        let user = {
-            username,
-            firstname,
-            lastname,
-            email,
-            password,
-        };
-        Service.signup(user);
-    }
+	const onSubmit = (e) => {
+		e.preventDefault();
+		let user = {
+			username,
+			firstname,
+			lastname,
+			email,
+			password,
+		};
+		Service.signup(user);
+	}
 
-    // let { username, firstname, lastname, email, password } = user;
-    return (
-        <div>
-            <Row xl="12" sm="12" xs="12" md="12">
-                <Col>
-                    <Card>
-                        <CardHeader>
-                            <h3>Đăng ký</h3>
-                        </CardHeader>
-                        <CardBody>
-                        <CardBody>
-                            <Form onSubmit={onSubmit}>
-                                <FormGroup>
-                                    <Label for="username">Tên đăng nhập</Label>
-                                    <Input name="username" onChange={onUserNameChange} required/>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="firstname">Họ</Label>
-                                    <Input name="firstname" onChange={onFirstnameChange} required/>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="lastname">Tên</Label>
-                                    <Input name="lastname" onChange={onLastnameChange} required/>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="email">Email</Label>
-                                    <Input name="email" onChange={onEmailChange} required/>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="password">Mật khẩu</Label>
-                                    <Input name="password" type="password" onChange={onPasswordChange} required/>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="rePassword">Nhập lại mật khẩu</Label>
-                                    <Input name="rePassword" type="password" onChange={onRePasswordChange} required/>
-                                </FormGroup>
-                                <button className="btn btn-primary" type="submit">Đăng ký</button>
-                            </Form>
-                        </CardBody>
-                        </CardBody>
-                        <CardFooter>
-                            <p>Đã có tài khoản? <a href="/login">Đăng nhập</a></p>
-                        </CardFooter>
-                    </Card>
-                </Col>
-            </Row>
-        </div>
-    )
+	return (
+		<div>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<div className="login-box">
+					<Typography component="h1" variant="h5">
+						Đăng ký tài khoản
+					</Typography>
+					<form className="login-form" noValidate>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									variant="outlined"
+									required
+									fullWidth
+									id="username"
+									label="Tên đăng nhập"
+									name="username"
+									autoComplete="username"
+									onChange={onUserNameChange}
+								/>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<TextField
+									autoComplete="fname"
+									name="firstName"
+									variant="outlined"
+									required
+									fullWidth
+									id="firstName"
+									label="Họ"
+									autoFocus
+									onChange={onFirstnameChange}
+								/>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<TextField
+									variant="outlined"
+									required
+									fullWidth
+									id="lastName"
+									label="Tên"
+									name="lastName"
+									autoComplete="lname"
+									onChange={onLastnameChange}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									variant="outlined"
+									required
+									fullWidth
+									id="email"
+									label="Địa chỉ email"
+									name="email"
+									autoComplete="email"
+									onChange={onEmailChange}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									variant="outlined"
+									required
+									fullWidth
+									name="password"
+									label="Mật khẩu"
+									type="password"
+									id="password"
+									autoComplete="current-password"
+									onChange={onPasswordChange}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									variant="outlined"
+									required
+									fullWidth
+									name="rePassword"
+									label="Nhập lại mật khẩu"
+									type="password"
+									id="rePassword"
+									autoComplete="current-password"
+									onChange={onRePasswordChange}
+								/>
+							</Grid>
+						</Grid>
+						<Button
+							className="btn-login"
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							onClick={onSubmit}
+						>
+							Đăng ký
+						</Button>
+						<Grid container justify="flex-end">
+							<Grid item>
+								<Link href="/login" variant="body2">
+									Bạn đã có mật khẩu? Đăng nhập.
+								</Link>
+							</Grid>
+						</Grid>
+					</form>
+				</div>
+				<Box mt={5}>
+				</Box>
+			</Container>
+		</div>
+	)
 }
