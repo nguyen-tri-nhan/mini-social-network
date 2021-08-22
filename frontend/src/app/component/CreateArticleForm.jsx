3389456be48f11b07d5c04b492478c462558a495
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-08-22 19:52:26 +0700

    add create article dialog

--- frontend/src/app/component/CreateArticleForm.jsx
+++ frontend/src/app/component/CreateArticleForm.jsx
@@ -2,19 +2,58 @@ import { useState } from "react";
 import {
   TextField,
   TextareaAutosize,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Button,
 } from '@material-ui/core';
 const CreateArticleForm = (props) => {
 
   const [outerInput, setOuterInput] = useState();
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const onCreateArticleOpenDialog = () => {
+    setOpenDialog(true);
+  }
+
+  const onCreateArticleCloseDialog = () => {
+    setOpenDialog(false);
+  }
+
+  const onArticleTextAreaChange = (e) => {
+    const value = e.target.value;
+    setOuterInput(value);
+  }
 
   return (
-    <div className="create-article-form">
-      <TextField 
-      label="What are you doing? Bro"
-      onClick="" 
-      value={outerInput} 
-      fullWidth/>
-    </div>
+    <>
+      <div className="create-article-form">
+        <TextField
+          label="What are you doing? Bro"
+          onClick={onCreateArticleOpenDialog}
+          value={outerInput}
+          fullWidth />
+      </div>
+      <Dialog open={openDialog} fullWidth maxWidth="lg">
+        <DialogContent>
+          <div className="col-xl-12 text-secondary">
+            <textarea
+              placeholder="Bạn đang nghĩ gì"
+              type="text"
+              className="form-control"
+              value={outerInput}
+              rows="7"
+              onChange={onArticleTextAreaChange}
+            />
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onCreateArticleCloseDialog}>
+            Huỷ
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
