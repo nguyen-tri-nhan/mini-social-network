db4be435e49b84ff6fe701df4f06d2bcb73bd7b9
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-08-25 21:29:38 +0700

    imgur

---
+++ frontend/src/app/helper/ImgurHelper.jsx
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+const apiKey = process.env.IMGUR_APIKEY;
+
+const ImgurHelper = () => {
+  const uploadImage = (image) => {
+    console.log(apiKey);
+  }
+}
+
+export default ImgurHelper;
