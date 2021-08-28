0443682783a5892993815a735b9b60cc00eb521f
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-08-29 00:03:01 +0700

    file type invalid

--- frontend/src/app/helper/ImgurHelper.jsx
+++ frontend/src/app/helper/ImgurHelper.jsx
@@ -4,14 +4,22 @@ const apiKey = process.env.REACT_APP_IMGUR_CLIENTID;
 const IMGUR_UPLOAD = 'https://api.imgur.com/3/image';
 
 const ImgurHelper = {
-  uploadImage(image) {
+  uploadImage(imageFile) {
+    let image;
+    toBase64(imageFile).then((result) => {
+      image = result;
+    })
     const config = {
       headers: {
         'Authorization': `Client-ID ${apiKey}`,
+        'Accept': 'application/json'
       },
       method: 'post',
       url: IMGUR_UPLOAD,
-      data: image,
+      data: {
+        image,
+        type: 'base64'
+      },
     };
     return new Promise((resolve, reject) => {
       axios(config)
@@ -22,5 +30,12 @@ const ImgurHelper = {
     })
   },
 }
+
+const toBase64 = file => new Promise((resolve, reject) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = () => resolve(reader.result);
+  reader.onerror = error => reject(error);
+});
 
 export default ImgurHelper;
