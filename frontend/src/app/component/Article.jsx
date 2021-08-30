329c4fb16f8ac1830eb844df326fde1cc3d5045e
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-08-30 23:56:38 +0700

    add newfeed view

--- frontend/src/app/component/Article.jsx
+++ frontend/src/app/component/Article.jsx
@@ -1,11 +1,18 @@
+import { Button } from 'reactstrap'
 
 const Article = (props) => {
-  const {article} = props;
+  const { article } = props;
   return (
-    <div>
-      <h1>
+    <div className="article">
+      <div className="article-owner d-flex">
+        <Button color='link'>{article.user.fullname}</Button>
+      </div>
+      <div className="article-desc d-flex">
         {article.description}
-      </h1>
+      </div>
+      <div className="article-image">
+        <img src={article.image} alt="" />
+      </div>
     </div>
   );
 };
