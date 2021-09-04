08c577fb9af6e034c9d64f49885a9fc2da9585f9
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-09-04 22:43:17 +0700

    add sticky box

--- frontend/src/app/component/LeftSideBar.jsx
+++ frontend/src/app/component/LeftSideBar.jsx
@@ -2,24 +2,27 @@ import { List, ListItem, ListItemIcon } from '@material-ui/core';
 import { Group } from '@material-ui/icons';
 import MContext from '../model/MContext';
 import ArticleOwner from './ArticleOwner';
+import StickyBox from 'react-sticky-box';
 const LeftSideBar = (props) => {
 
   const user = MContext.user;
-  
+
   return (
-    <div>
-      <List component="nav">
-        <ListItem button>
-          <ArticleOwner user={user} />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <Group />
-            Bạn bè
-          </ListItemIcon>
-        </ListItem>
-      </List>
-    </div>
+    <StickyBox offsetTop={42} offsetBottom={20}>
+      <div className="left-side-bar">
+        <List component="nav">
+          <ListItem button>
+            <ArticleOwner user={user} />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <Group />
+              Bạn bè
+            </ListItemIcon>
+          </ListItem>
+        </List>
+      </div>
+    </StickyBox>
   )
 };
 
