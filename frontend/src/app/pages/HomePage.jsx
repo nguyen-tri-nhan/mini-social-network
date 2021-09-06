519bac69fc1567f64ab239f0792ebf06b213ca23
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-09-06 16:00:10 +0700

    break home page

--- frontend/src/app/pages/HomePage.jsx
+++ frontend/src/app/pages/HomePage.jsx
@@ -9,14 +9,18 @@ const HomePage = (props) => {
   return (
     <>
       <Row>
-        <Col md={3} lg={4}>
+        <Col md={3} lg={2}>
           <LeftSideBar />
+        </Col>
+        <Col md={0} lg={2}>
         </Col>
         <Col md={6} lg={4}>
           <CreateArticleForm />
           <NewsFeed />
         </Col>
-        <Col md={3} lg={4}>
+        <Col md={0} lg={2}>
+        </Col>
+        <Col md={3} lg={2}>
         </Col>
       </Row>
     </>
