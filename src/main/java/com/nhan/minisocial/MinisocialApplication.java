44eb276e9bfbb79cad9300a228549d2142ad1580
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-10-23 21:40:35 +0700

    clean main code

--- src/main/java/com/nhan/minisocial/MinisocialApplication.java
+++ src/main/java/com/nhan/minisocial/MinisocialApplication.java
@@ -1,13 +1,8 @@
 package com.nhan.minisocial;
 
-import com.nhan.minisocial.core.entity.Role;
-import com.nhan.minisocial.core.entity.RoleName;
-import com.nhan.minisocial.core.repository.RoleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.annotation.PostConstruct;
@@ -20,12 +15,10 @@ import java.util.TimeZone;
 })
 public class MinisocialApplication {
 
-
     @PostConstruct
     void init(){
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
-
 
     public static void main(String[] args) {
         SpringApplication.run(MinisocialApplication.class, args);
